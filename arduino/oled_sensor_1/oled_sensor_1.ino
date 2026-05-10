ac7d557727fd1d4aca19886c163ec4e1272b9ee3
Gino Curtis <ginocurtis5@gmail.com>
2026-05-09 20:17:19 -0700

    feat: temp and RH sensor 1

--- arduino/oled_sensor_1/oled_sensor_1.ino
+++ arduino/oled_sensor_1/oled_sensor_1.ino
@@ -61,7 +61,7 @@ void loop() {
   display.clearDisplay();
 
   display.setCursor(0, 0);
-  display.println("SENSOR 1);
+  display.println("SENSOR 1");
   display.setCursor(0, 24);
   display.print("Temp: ");
   display.print(tempF);
